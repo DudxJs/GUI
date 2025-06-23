bdef05fc62ebd3b8998954781e97c46ee4605180
DudxJs <eduardosantschocolate@gmail.com>
2025-06-22 23:11:05 -0300

    update ChatSpy.lua

--- ChatSpy.lua
+++ ChatSpy.lua
@@ -234,7 +234,7 @@ UIListLayout.Padding = UDim.new(0, 7)
 
 -- Funções de rolagem inteligente
 local function IsAtBottom()
-    local tolerance = 15
+    local tolerance = 90
     return MessageList.CanvasPosition.Y + MessageList.AbsoluteWindowSize.Y >= MessageList.AbsoluteCanvasSize.Y - tolerance
 end
 
